bb11d82a3cf24d8530535f5816ab3d8a75fa0b96
Ilya Baldin <baldin@jlab.org>
2024-05-28 16:49:25 -0400

    Updating the notebook to use source-built Boost

--- scripts/notebooks/EJFAT/post-boot/recver.sh
+++ scripts/notebooks/EJFAT/post-boot/recver.sh
@@ -2,7 +2,7 @@
 
 # install missing software
 sudo apt-get -yq update
-sudo apt-get -yq install python3-pip build-essential autoconf cmake libtool pkg-config libglib2.0-dev libboost-all-dev ninja-build openssl libssl-dev libsystemd-dev protobuf-compiler libre2-dev
+sudo apt-get -yq install python3-pip build-essential autoconf cmake libtool pkg-config libglib2.0-dev ninja-build openssl libssl-dev libsystemd-dev protobuf-compiler libre2-dev
 
 # install meson
 pip3 install --user meson
