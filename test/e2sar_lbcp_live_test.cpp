0bb05789f92f0fe79cfc18e7053bf8629120f47f
Ilya Baldin <baldin@jlab.org>
2024-06-06 17:27:08 -0400

    Checking for data address

--- test/e2sar_lbcp_live_test.cpp
+++ test/e2sar_lbcp_live_test.cpp
@@ -39,7 +39,8 @@ BOOST_AUTO_TEST_CASE(LBMLiveTest1)
     BOOST_CHECK(!res.has_error());
     BOOST_CHECK(!lbman.get_URI().get_InstanceToken().value().empty());
     BOOST_CHECK(lbman.get_URI().has_syncAddr());
-    BOOST_CHECK(lbman.get_URI().has_dataAddr());
+    // don't expect data address in a null setup
+    //BOOST_CHECK(lbman.get_URI().has_dataAddr());
 
     // call free
     res = lbman.freeLB();
