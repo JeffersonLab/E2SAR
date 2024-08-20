d5c4df7429183bd19f5ee5600c14f74139c995f4
Ilya Baldin <baldin@jlab.org>
2024-08-20 15:14:04 -0400

    Added MemoryError and LogicError return codes

--- include/e2sarError.hpp
+++ include/e2sarError.hpp
@@ -32,7 +32,8 @@ namespace e2sar
     NotFound = 7,
     RPCError = 8,
     SocketError = 9,
-    MemoryError = 10
+    MemoryError = 10,
+    LogicError = 11
   };
 
   struct E2SARErrorInfo
@@ -118,6 +119,8 @@ namespace detail
         return "socket error";
       case e2sar::E2SARErrorc::MemoryError:
         return "memory error";
+      case e2sar::E2SARErrorc::LogicError:
+        return "logic error";
       default:
         return "unknown";
       }
