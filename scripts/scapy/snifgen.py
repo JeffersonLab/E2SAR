60ae4567618e442eff5d4492f2747bc9406f0dd6
Ilya Baldin <baldin@jlab.org>
2024-09-29 22:55:00 -0400

    Cleaned up options; added --lbresync combined LBRE+Sync packets listen and parse; disambiguated syncports and data ports

--- scripts/scapy/snifgen.py
+++ scripts/scapy/snifgen.py
@@ -106,7 +106,7 @@ def genLBREPkt(ip_addr: str, udp_port: int, entropy: int, dataId: int, eventNumb
     max_segment_len = mtu - LBPacketLength - REPacketLength
     if max_segment_len < 0:
         print(f'MTU of {mtu} too short to accommodate LB header {LBPacketLength} and RE header {REPacketLength}')
-        sys.exit()
+        sys.exit(-1)
     segment_offset = 0
     segment_end = len(payload) if len(payload) < max_segment_len else max_segment_len
     while segment_offset < len(payload):
@@ -129,7 +129,7 @@ def genREPkt(ip_addr: str, udp_port: int, dataId: int, eventNumber: int, payload
     max_segment_len = mtu - REPacketLength
     if max_segment_len < 0:
         print(f'MTU of {mtu} too short to accommodate RE header {REPacketLength}')
-        sys.exit()
+        sys.exit(-1)
     segment_offset = 0
     segment_end = len(payload) if len(payload) < max_segment_len else max_segment_len
     while segment_offset < len(payload):
@@ -148,8 +148,8 @@ def genREPkt(ip_addr: str, udp_port: int, dataId: int, eventNumber: int, payload
 def validate_sync_packet(packet):
     if not (packet.preamble == b'LC'):
         return False, f"Preamble must be 'LC' instead of {packet.preamble}"
-    if not (packet.version == 1):
-        return False, f"Expected version number 1, not {packet.version}"
+    if not (packet.version == 2):
+        return False, f"Expected version number 2, not {packet.version}"
     return True, ""
 
 # validate LB packet
@@ -201,29 +201,30 @@ def packet_callback(packet):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
-
     operations = parser.add_mutually_exclusive_group(required=True)
     operations.add_argument("-l", "--listen", action="store_true", help="listen for incoming packets and try to parse and validate them")
     operations.add_argument("-g", "--generate", action="store_true", help="generate new packets of specific types")
     operations.add_argument("-a", "--parse", action="store_true", help="parse a pcap file. The recommended way to capture is something like this 'sudo tcpdump -s 200 -tttt -i enp7s0 udp \\( dst port 19522 or dst port 19523 \\) -w e2sar.pcap'")
-    parser.add_argument("-p", "--port", action="store", help="UDP port (for -l and -g)", default=19522, type=int)
-    parser.add_argument("-n", "--nports", action="store", type=int, default=1, help="number of ports starting with -p to listen on")
-    parser.add_argument("-c", "--count", action="store", help="number of packet streams (if pld larger than mtu, otherwise packets) to generate or expect or parse", default=10, type=int)
+    parser.add_argument("-p", "--port", action="store", default=19522, type=int, help="UDP data port (only port for --lbre, starting port for --re)")
+    parser.add_argument("-y", "--syncport", action="store", help="UDP sync port", default=19010, type=int)
+    parser.add_argument("-n", "--nports", action="store", type=int, default=1, help="number of ports starting with -p to listen on for --re")
+    parser.add_argument("-c", "--count", action="store", default=10, type=int, help="number of events (if pld larger than mtu, otherwise packets) to generate or expect or parse")
     parser.add_argument("--ip", action="store", help="IP address to which to send the packet(s) or listen from")
     parser.add_argument("--show", action="store_true", default=False, help="only show the packet without sending it (with -g)")
-    parser.add_argument("--entropy", action="store", default=0, help="entropy value for LB+RE packet", type=int)
-    parser.add_argument("--event", action="store", default=0, help="event number for sync, LB+RE and RE packet", type=int)
+    parser.add_argument("--entropy", action="store", default=0, type=int, help="entropy value for LB+RE packet")
+    parser.add_argument("--event", action="store", default=0, type=int, help="event number for sync, LB+RE and RE packet")
     parser.add_argument("--rate", action="store", default=100, type=int, help="event rate in Hz for Sync packet")
     parser.add_argument("--dataid", action="store", default=1, type=int, help="data id for RE packet")
     parser.add_argument("--srcid", action="store", default=1, type=int, help="source id for Sync packet")
     parser.add_argument("--mtu", action="store", type=int, default=1500, help="set the MTU length, so LB+RE and RE packets can be fragmented.")
-    parser.add_argument("--pld", action="store", help="payload for LB+RE or RE packets. May be broken up if MTU size insufficient")
+    parser.add_argument("--pld", action="store", help="payload for LB+RE or RE packets. May be broken up if MTU size insufficient", default="This is a default payload.")
     parser.add_argument("--iface", action="store", default="all", help="which interface should we listen on (defaults to all)")
     parser.add_argument("-f", "--file", action="store", help="pcap file name to parse", default="./e2sar.pcap")
     packet_types = parser.add_mutually_exclusive_group(required=True)
-    packet_types.add_argument("--sync", action="store_true", help="listen for or generate sync packets")
-    packet_types.add_argument("--lbre", action="store_true", help="listen for or generate packets with LB+RE header")
-    packet_types.add_argument("--re", action="store_true", help="listen for or generate packets with just the RE header")
+    packet_types.add_argument("--sync", action="store_true", help="listen for, parse or generate Sync packets")
+    packet_types.add_argument("--lbre", action="store_true", help="listen for, parse or generate packets with LB+RE header")
+    packet_types.add_argument("--re", action="store_true", help="listen for, parse or generate packets with just the RE header")
+    packet_types.add_argument("--lbresync", action="store_true", help="listen for or parse LB+RE and Sync packets")
 
     args = parser.parse_args()
 
@@ -234,21 +235,35 @@ if __name__ == "__main__":
     if args.generate:
         if not args.ip:
             print(f'--ip option is required (use dotted notation to specify IPv4 address)')
-            sys.exit()
+            sys.exit(-1)
         # generate and show a packet
+        if args.lbresync:
+            print('Invalid option combination -g/--generate and --lbresync - multiple layers are bound, pick one')
+            sys.exit(-1)
+
+        if args.sync:
+            # sync packets
+            bind_sync_hdr(args.syncport)
+            print(f'Generating Sync(port {args.syncport}) packets for mtu {args.mtu}')
+        elif args.lbre:
+            # lb+re packets
+            bind_lb_hdr(args.port)
+            print(f'Generating LBRE(port {args.port}) packets for mtu {args.mtu}')
+        elif args.re:
+            # re packets (only one port)
+            bind_re_hdr(args.port)
+            print(f'Generating RE(port {args.port}) packets for mtu {args.mtu}')
+
         packets = list()
         for i in range(args.count):
             if args.sync:
                 # sync packets
-                bind_sync_hdr(args.port)
                 packets.append(genSyncPkt(args.ip, args.port, args.srcid, args.event + i, args.rate))
             elif args.lbre:
                 # lb+re packets
-                bind_lb_hdr(args.port)
                 packets.extend(genLBREPkt(args.ip, args.port, args.entropy, args.dataid, args.event, args.pld, args.mtu))
             elif args.re:
-                # re packets
-                bind_re_hdr(args.port)
+                # re packets (only one port)
                 packets.extend(genREPkt(args.ip, args.port, args.dataid, args.event, args.pld, args.mtu))
 
         if args.show:
@@ -269,15 +284,23 @@ if __name__ == "__main__":
 
         if args.sync:
             # sync packets
-            bind_sync_hdr(args.port)
-            packet_type = 'Sync'
+            bind_sync_hdr(args.syncport)
+            packet_type = f'Sync(port {args.syncport})'
         elif args.lbre:
             # lb+re packets
             bind_lb_hdr(args.port)
-            packet_type = 'LB+RE'
+            packet_type = f'LB+RE(port {args.port})'
         elif args.re:
-            bind_re_hdr([x + args.port for x in range(0, args.nports)])
-            packet_type = 'RE'
+            # multiple ports possible
+            portList = [x + args.port for x in range(0, args.nports)]
+            bind_re_hdr(portList)
+            packet_type = f'RE(ports {portList})'
+        elif args.lbresync:
+             # sync packets
+            bind_sync_hdr(args.syncport)
+            # lb+re packets on a single port
+            bind_lb_hdr(args.port)
+            packet_type = f'LB+RE(port {args.port}) + Sync(port {args.syncport})'   
 
         # Linux supports "any" shortcut interface specification, but other OSs may not, so we just list
         # all the interfaces first
@@ -294,16 +317,23 @@ if __name__ == "__main__":
 
         if args.sync:
             # sync packets
-            bind_sync_hdr(args.port)
-            packet_type = 'Sync'
+            bind_sync_hdr(args.syncport)
+            packet_type = f'Sync(port {args.syncport})'
         elif args.lbre:
             # lb+re packets
             bind_lb_hdr(args.port)
-            packet_type = 'LB+RE'
+            packet_type = f'LB+RE(port {args.port})'
         elif args.re:
-            #bind_re_hdr(args.port)
-            bind_re_hdr([x + args.port for x in range(0, args.nports)])
-            packet_type = 'RE'
+            # multiple ports possible
+            portList = [x + args.port for x in range(0, args.nports)]
+            bind_re_hdr(portList)
+            packet_type = f'RE(ports {portList})'
+        elif args.lbresync:
+             # sync packets
+            bind_sync_hdr(args.syncport)
+            # lb+re packets on a single port
+            bind_lb_hdr(args.port)
+            packet_type = f'LB+RE(port {args.port}) + Sync(port {args.syncport})'    
 
         print(f'Looking for {packet_type} packets in PCAP file {args.file}')
         try:
