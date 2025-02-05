8ceb26245385981004e0d3477e5d44c85a019d8a
Ilya Baldin <baldin@jlab.org>
2025-02-05 11:04:00 -0500

    Increasing the size of SQE queue

--- include/e2sarDPSegmenter.hpp
+++ include/e2sarDPSegmenter.hpp
@@ -74,7 +74,7 @@ namespace e2sar
             struct io_uring ring;
             // each ring has to have a predefined size - we want to
             // put at least 2*eventSize/bufferSize entries onto it
-            const size_t uringSize = 256;
+            const size_t uringSize = 1000;
 
             // data carried in sqe, returned in cqe
             // mainly for memory management
