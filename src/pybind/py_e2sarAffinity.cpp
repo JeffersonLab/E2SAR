27d524d4a9dc8716eba163238faee840cbf0bd7b
Xinxin (Cissie) Mei <xinxinmei89@gmail.com>
2025-02-28 02:06:30 -0500

    Add binding for e2sar::Affinity

---
+++ src/pybind/py_e2sarAffinity.cpp
@@ -0,0 +1,29 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include <pybind11/operators.h>
+
+#include "e2sarAffinity.hpp"
+
+namespace py = pybind11;
+using namespace e2sar;
+
+
+void init_e2sarAffinity(py::module_ &m) {
+    py::class_<Affinity>(m, "Affinity")
+        .def_static(
+            "set_process", &Affinity::setProcess,
+            py::arg("cores"),
+            "Set the affinity of the entire process to the cores in the vector")
+        .def_static(
+            "set_thread", &Affinity::setThread,
+            py::arg("core"),
+            "Set calling thread affinity to the specified core.")
+        .def_static(
+            "set_thread_xor", &Affinity::setThreadXOR,
+            py::arg("cores"),
+            "Set calling thread affinity to exclude specified cores.")
+        .def_static(
+            "set_numa_bind", &Affinity::setNUMABind,
+            py::arg("node"),
+            "Bind process memory allocation to the specified NUMA node.");
+}
