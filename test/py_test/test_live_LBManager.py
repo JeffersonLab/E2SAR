7fae0cda7e9b4a80ccdeeab643c11fe848319c43
Xinxin (Cissie) Mei <xinxinmei89@gmail.com>
2025-03-03 16:48:47 -0500

    Add 3 new LBmanager methods

--- test/py_test/test_live_LBManager.py
+++ test/py_test/test_live_LBManager.py
@@ -1,5 +1,5 @@
 """
-The pytest for cpp class e2sar::EjfatURI.
+The pytest for cpp class e2sar::LBManager.
 
 To make sure it's working, either append the path of "e2say_py.*.so" to sys.path. E.g,
 # import sys
@@ -15,20 +15,26 @@ import pytest
 
 # Make sure the compiled module is added to your path
 import e2sar_py
+lbm = e2sar_py.ControlPlane.LBManager
 ej_uri = e2sar_py.EjfatURI
 
-URI_STR = "ejfat://token@192.188.29.6:18020/lb/36?sync=192.188.29.6:19020&data=192.188.29.20"
+URI_STR = "ejfats://udplbd@192.168.0.3:18347/lb/1?data=127.0.0.1&sync=192.168.88.199:1234"
 
-def test_get_dp_local_addrs():
-    """Test the get_dp_local_addr function."""
+def test_add_sender_self():
+    """Test the add_sender_self function."""
 
     uri = ej_uri(URI_STR, ej_uri.TokenType.instance)
     assert isinstance(uri, ej_uri), "EjfatURI creation failed!"
 
-    ips = uri.get_dp_local_addrs(False)
-    assert isinstance(ips, list), "EjfatURI get_dp_local_addrs failed!"
-    # More strict validation: ips[0] equals to local IP address regardless of URI_STR
+    lb_manager = lbm(uri, False)
+    assert isinstance(lb_manager, lbm), "LBManager creation error! "
+
+    res = lb_manager.add_sender_self(False)
+    assert isinstance(res, e2sar_py.E2SARResultInt), "LBManager add_sender_self error! "
+
+    # TODO: require real LB to test the functionality
 
 
 if __name__ == "__main__":
     pytest.main()
+    # test_add_sender_self()
