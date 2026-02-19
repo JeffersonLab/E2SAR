2b24ce6b31055bd6cf98adab6a0acded39c92304
Ilya Baldin <baldin@jlab.org>
2026-02-18 23:14:01 -0500

    Another minor leak on exit - lost event queue draining

--- include/e2sarDPReassembler.hpp
+++ include/e2sarDPReassembler.hpp
@@ -679,6 +679,12 @@ namespace e2sar
                     } while (a);
 
                     gcThreadState.threadObj.join();
+
+                    // drain lost events queue - tuples are heap-allocated in logLostEvent
+                    // and only freed by get_LostEvent(); if the caller never calls it they leak
+                    boost::tuple<EventNum_t, u_int16_t, size_t>* evtPtr{nullptr};
+                    while (recvStats.lostEventsQueue.pop(evtPtr))
+                        delete evtPtr;
                 }
             }
         protected:
