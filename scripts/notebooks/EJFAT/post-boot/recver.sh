fb882539e766069a3b84cdc8040cad9ee829a7fd
Ilya Baldin <baldin@jlab.org>
2026-09-09 12:17:56 -0400

    fix: Updated the development tester notebook for udplbd2 as well as 0.4.0 changes

--- scripts/notebooks/EJFAT/post-boot/recver.sh
+++ scripts/notebooks/EJFAT/post-boot/recver.sh
@@ -3,7 +3,7 @@
 # get the distro (ubuntu or rocky)
 distro=`awk 'BEGIN { FS = "=" } ; /^ID=/ {gsub(/"/, ""); print $2}' /etc/*-release`
 
-if [[ ${distro} == 'ubuntu' ]]; then 
+if [[ ${distro} == 'ubuntu' ]]; then
     echo Installing for Ubuntu
 
     # need to install python3.10 on ubuntu20.04
@@ -19,31 +19,28 @@ if [[ ${distro} == 'ubuntu' ]]; then
         sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 1
         sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 2
     fi
-    
+
     # install missing software
     sudo apt-get -yq update
     sudo dpkg -r ufw
-    sudo apt-get -yq install python3-pip build-essential autoconf cmake libtool pkg-config libglib2.0-dev ninja-build openssl libssl-dev libsystemd-dev protobuf-compiler libre2-dev gdb docker.io firewalld
-    
+    sudo apt-get -yq install python3-pip build-essential autoconf cmake libtool pkg-config libglib2.0-dev ninja-build openssl libssl-dev libsystemd-dev protobuf-compiler libre2-dev gdb docker.io firewalld unzip liburing-dev liburing2
+
     # install meson
     if [[ ${version} == '24.04' ]]; then
         pip3 install --user --break-system-packages meson pybind11
     else
         pip3 install --user meson pybind11
     fi
+
     # install scapy system-wide as root needs to run it
     pip3 install scapy
 
     sudo usermod -a -G docker ubuntu
+    echo "export DOCKER_CONFIG=$HOME/.docker" >> ~/.profile
 
-    # install git-lfs
-    curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
-    sudo apt-get install git-lfs
-    git lfs install 
-    
     # enable apport so core dumps can be caught under /var/lib/apport/coredump/
     sudo systemctl enable apport.service
-    
+
 fi
 
 if [[ ${distro} == 'rocky' ]]; then
@@ -53,29 +50,34 @@ if [[ ${distro} == 'rocky' ]]; then
     sudo dnf -yq --enablerepo=devel update
     sudo dnf -yq --enablerepo=devel install gcc gcc-c++ kernel-devel make
     # libssl-dev libsystemd-dev
-    sudo dnf -yq --enablerepo=devel install python3-pip autoconf cmake libtool pkg-config ninja-build openssl protobuf-compiler gdb git glib2-devel re2-devel libquadmath-devel python39-devel python39
+    sudo dnf -yq --enablerepo=devel install python3-pip autoconf cmake libtool pkg-config ninja-build openssl protobuf-compiler gdb git glib2-devel re2-devel libquadmath-devel python39-devel python39 liburing-devel liburing
 
     sudo update-alternatives --set python /usr/bin/python3.9
     sudo update-alternatives --set python3 /usr/bin/python3.9
-    #install meson 
+    #install meson
     pip3 install --user meson pybind11
     # install scapy system-wide as root needs to run it
     pip3 install scapy
 
-    # install git-lfs
-    curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.rpm.sh | sudo bash
-    sudo dnf -yq install git-lfs
-    
+    sudo systemctl start docker
+    sudo usermod -a -G docker rocky
+    echo "export DOCKER_CONFIG=$HOME/.docker" >> ~/.bashrc
+    echo "export DOCKER_CONFIG=$HOME/.docker" >> ~/.profile
+
 fi
 
 # install docker compose and buildx bits
 export DOCKER_CONFIG=$HOME/.docker
 mkdir -p ${DOCKER_CONFIG}/cli-plugins
-curl -SL https://github.com/docker/compose/releases/download/v2.27.0/docker-compose-linux-x86_64   -o $DOCKER_CONFIG/cli-plugins/docker-compose 
+curl -SL https://github.com/docker/compose/releases/download/v2.27.0/docker-compose-linux-x86_64   -o $DOCKER_CONFIG/cli-plugins/docker-compose
 chmod +x .docker/cli-plugins/docker-compose
 
 curl -SL https://github.com/docker/buildx/releases/download/v0.14.0/buildx-v0.14.0.linux-amd64  -o $DOCKER_CONFIG/cli-plugins/docker-buildx
 chmod +x .docker/cli-plugins/docker-buildx
+
+# make docker socket world-accessible so persistent SSH sessions (e.g. paramiko)
+# can use docker without picking up group membership changes made after connect
+sudo chmod 666 /var/run/docker.sock
 
 # put cpnode in etc hosts to enable testing certificate validation
 echo "192.168.0.3 cpnode" | sudo tee -a /etc/hosts
